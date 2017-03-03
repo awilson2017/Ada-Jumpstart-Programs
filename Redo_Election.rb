c3d58c244038a5368a820a28fd48da2d8a49f986
awilson2017 <wilsonange@gmail.com>
2017-03-02 21:42:46 -0800

    Update Redo_Election.rb

--- Redo_Election.rb
+++ Redo_Election.rb
@@ -22,8 +22,10 @@ Please enter your vote."
 
 # add votes to tally
 (1..number_votes).each do |x|
+  # Capitolize all words from user input (http://stackoverflow.com/questions/13520162/ruby-capitalize-every-word-first-letter)
   user_vote = gets.chomp.split.map { |x| x.capitalize }.join(' ')
   puts "Voter ##{x}: #{user_vote}"
+    # Boolean condition for check user input is already a key in hash (https://docs.ruby-lang.org/en/2.0.0/Hash.html#method-i-has_key-3F)
     if candidates.has_key?(user_vote)
       candidates[user_vote] += 1
     else
@@ -38,8 +40,8 @@ puts "ELECTION RESULTS....
 
 Vote Summary:"
 
-candidates.each do |k, v|
-  puts "#{k} - #{v} #{(v == 1 ? "vote" : "votes")}"
+candidates.each do |k, v| 
+  puts "#{k} - #{v} #{(v == 1 ? "vote" : "votes")}" #https://books.google.com/books?id=jcUbTcr5XWwC&pg=PA127&lpg=PA127&dq=handle+grammar+singular+plural+ruby&source=bl&ots=fJIhAe9uiF&sig=yFDiaPEw2xKlvClFOKelnpU92v0&hl=en&sa=X&ved=0ahUKEwjg3vG80frQAhXByVQKHajJDowQ6AEINjAE#v=onepage&q=handle%20grammar%20singular%20plural%20ruby&f=false
 end
 
 # account for ties
